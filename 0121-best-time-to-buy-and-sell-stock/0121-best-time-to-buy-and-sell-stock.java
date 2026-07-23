75ffc9500b3da350ca29699580210016aab2dfde
Darshan Sanjay Bhaiyya <darshanbhaiyya05@gmail.com>
2026-07-23 21:17:29 +0530

    Time: 2 ms (42.35%), Space: 94.4 MB (51.51%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bday=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+                if(prices[i]<bday){
+                    bday=prices[i];
+                }
+                else if(profit<prices[i]-bday){
+                    profit=prices[i]-bday;
+                }
+            }
+            return profit;
+    }
+}
