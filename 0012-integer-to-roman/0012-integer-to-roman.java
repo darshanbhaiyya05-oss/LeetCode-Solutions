6ea3ef93a31a8072d9fc7ac085bfcaf954515dfb
Darshan Sanjay Bhaiyya <darshanbhaiyya05@gmail.com>
2026-08-24 15:00:39 +0530

    Time: 3 ms (99.81%), Space: 46.2 MB (78.59%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Solution {
-    public static String intToRoman(int num) {
+    public  String intToRoman(int num) {
         // Roman numeral symbols with their values
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
@@ -16,13 +16,5 @@ class Solution {
             }
         }
         return roman.toString();
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        sc.close(); // Close Scanner
-
-        System.out.println(intToRoman(num));
     }
 }
